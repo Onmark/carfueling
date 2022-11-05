315020e30e8f87d4a913ef301127b306094043b1
Onmark <marko.on@seznam.cz>
2022-11-05 19:24:17 +0100

    adding .war packaging

--- src/main/java/com/markovic/carfueling/CarfuelingApplication.java
+++ src/main/java/com/markovic/carfueling/CarfuelingApplication.java
@@ -2,9 +2,10 @@ package com.markovic.carfueling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CarfuelingApplication {
+public class CarfuelingApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CarfuelingApplication.class, args);
